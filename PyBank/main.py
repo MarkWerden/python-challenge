d58769fafe7aad7daa074c0d44de9552dd87cc68
MarkWerden <markwerden@yahoo.com>
2022-02-01 11:27:21 -0800

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,65 +1,73 @@
-# Important to note, some mechanics of this code was taken from outside sources. In an attempt
-# to show that I am understanding it rather than just copy-pasting, I attempted to explain where
-# I can. - Mark
+# Some of the code used is based off of what I learned from the Bank assignment and outside sources.
+# Like before, I will attempt to explain where I can. Some things I did not explain here are most
+# likely explained in the other assignment or was something I learned in class. - Mark
 
 import os
 import csv
 
-file = "Resources/budget_data.csv"
+file = "Resources/election_data.csv"
 
-TotalMonths = 1 # Defined as 1 because it ignores the header row
 Total = 0
-MonthlyChange = [] # Defined as a list beccause of its appending function 
-GreatestIncrease = 0
-GreatestDecrease = 0
-GMI = 0 # Short for "Greatest Month Increase"
-        # its the variable that will hold the associated month when profits are highest
-GMD = 0 # Short for "Greatest Month Decrease" 
-        # its the variable that will hold the associated month when profits are lowest
+Votes_Khan = 0
+Votes_Correy = 0   # Each represenative has to have their own counter for it to count their own votes.
+Votes_Li = 0
+Votes_oTooley = 0
 
 with open(file) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    
-    csv_header = next(csvreader)
-    row = next(csvreader) # Defines the value "Row" as any value that isn't the header
-    PreviousRow = int(row[1]) # Defines the value based off of what the "Row" value is (for finding the average)
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvfile)
     
     for row in csvreader:
-        TotalMonths = TotalMonths + 1 
-        Total = Total + int(row[1])
+        Total = Total + 1
     
-        RevenueChange = int(row[1]) - PreviousRow # Subtracts the value defined by the row and the "PreviousRow" variable
-        MonthlyChange.append(RevenueChange) # Adds the value to the associated list
-        PreviousRow = int(row[1]) # A means to "reset" the variable for it not to incorrectly calculate the changes
-        
-        if int(row[1]) > GreatestIncrease: # This "if" statement compares the value of the cell to the value of the variable.
-            GreatestIncrease = int(row[1]) # If it is higher than it, it will replace the value with the new one and take
-            GMI = row[0]                   # the associated month in that row.
+        if (row[2] == "Khan"):
+            Votes_Khan = Votes_Khan + 1    # This "if/else" statement checks to see if the name column
+        elif (row[2] == "Correy"):         # has their name in that row. If so, it adds it to the list.
+            Votes_Correy = Votes_Correy + 1  
+        elif (row[2] == "Li"):
+            Votes_Li = Votes_Li + 1
+        else:
+            Votes_oTooley = Votes_oTooley + 1
             
-        if int(row[1]) < GreatestDecrease: 
-            GreatestDecrease = int(row[1]) # The same logic applies to this "if" statement as well
-            GMD = row[0]
-        
-    AverageChange = sum(MonthlyChange)/len(MonthlyChange) # Takes the average of the changes within the list, using the list created by appending.
+    Percent_Khan = Votes_Khan / Total      # Pretty self-explanatory, its finding the representative's
+    Percent_Correy = Votes_Correy / Total  # votes compared to the total in decimal form.
+    Percent_Li = Votes_Li / Total
+    Percent_oTooley = Votes_oTooley / Total
     
-    Maximum = max(MonthlyChange) # These two are essentially the same as using Excel formulas
-    Minimum = min(MonthlyChange)
+    ElectionWinner = max(Votes_Khan, Votes_Correy, Votes_Li, Votes_oTooley)
     
-    print(f"Financial Analysis")
-    print(f"---------------------------")
-    print(f"Total Months: {TotalMonths}")
-    print(f"Total: ${Total}")
-    print(f"Average Change: ${AverageChange:.2f}")  # The "2f" is saying to stop at 2 decimal places.
-    print(f"Greatest Increase in Profits:, {GMI}, (${Maximum})")
-    print(f"Greatest Decrease in Profits:, {GMD}, (${Minimum})")
+    if ElectionWinner == Votes_Khan:
+        Winner = "Khan"
+    elif ElectionWinner == Votes_Correy: # This "if/else" statement determines who is the winner
+        Winner = "Correy"                # based off of the max value collected by the variable.
+    elif ElectionWinner == Votes_Li:
+        Winner = "Li"
+    else:
+        Winner = "O'Tooley"
 
-output = "Analysis/ExportedBudgetData.txt"
+print(f"Election Results")
+print(f"---------------------------")
+print(f"Total Votes: {Total}")
+print(f"---------------------------")
+print(f"Khan: {Percent_Khan:.3%} ({Votes_Khan})") # The ".3%" is telling it to stop at 3 decimal places.
+print(f"Correy: {Percent_Correy:.3%} ({Votes_Correy})")
+print(f"Li: {Percent_Li:.3%} ({Votes_Li})")
+print(f"O'Tooley: {Percent_oTooley:.3%} ({Votes_oTooley})")
+print(f"---------------------------")
+print(f"Winner: {Winner}")
+print(f"---------------------------")
+
+output = "Analysis/ExportedElectionData.txt"
 
 with open(output, 'w') as txtfile:
-    txtfile.write(f"Financial Analysis\n")
-    txtfile.write(f"---------------------------\n")  # "\n" is essentially telling the script to start it on a new line
-    txtfile.write(f"Total Months: {TotalMonths}\n")
-    txtfile.write(f"Total: ${Total}\n")
-    txtfile.write(f"Average Change: ${AverageChange:.2f}\n")
-    txtfile.write(f"Greatest Increase in Profits:, {GMI}, (${Maximum})\n")
-    txtfile.write(f"Greatest Decrease in Profits:, {GMD}, (${Minimum})\n")
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Votes: {Total}\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Khan: {Percent_Khan:.3%} ({Votes_Khan})\n")
+    txtfile.write(f"Correy: {Percent_Correy:.3%} ({Votes_Correy})\n")
+    txtfile.write(f"Li: {Percent_Li:.3%} ({Votes_Li})\n")
+    txtfile.write(f"O'Tooley: {Percent_oTooley:.3%} ({Votes_oTooley})\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Winner: {Winner}\n")
+    txtfile.write(f"---------------------------\n")
